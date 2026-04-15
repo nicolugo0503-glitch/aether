bd8784bdd2529a6bee7cc1c8f2b84f5808ede09c
Nicolas Lugo <nicolugo0503@gmail.com>
2026-04-14 22:20:58 -0600

    fix stripe api version

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "sk_test_placeholder", {
-  apiVersion: "2024-10-28.acacia",
+  apiVersion: "2025-02-24.acacia",
   typescript: true,
   appInfo: { name: "Aether", version: "0.1.0" },
 });
